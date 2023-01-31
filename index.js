de3933c8633e8fa0341daac50194b85327436643
Maxim Kuzmianok <jst.wrx@gmail.com>
2023-01-31 19:45:12 +0300

    feat: add models and connect DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const express = require('express')
+const sequelize = require('./config/db')
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -10,6 +11,13 @@ app.use(cors())
 
 const start = async () => {
     try {
+        await sequelize.authenticate().then(() => {
+            console.log("DATABASE CONNECTED! ");
+        })
+            .catch(err => {
+                console.log(err);
+            })
+        await sequelize.sync()
         app.listen(PORT, () => console.log(`server started, port:${PORT}`))
     } catch (error) {
         console.log(error)
