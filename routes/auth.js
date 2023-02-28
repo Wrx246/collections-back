2a1286e92c53b97fe1b10fd73fe6eceb6b8d46bc
Maxim Kuzmianok <jst.wrx@gmail.com>
2023-02-28 12:28:37 +0300

    fix: add route for userControl

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/auth')
 
 routerAuth.post('/registration', authController.registration)
 routerAuth.post('/login', authController.login)
+routerAuth.post('/check', authController.checkBan)
 
 module.exports = routerAuth
